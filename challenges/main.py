3a1d0bc8546ad21e2511bd71b59094c245b2aed3
Almubee <144207594+Almubee@users.noreply.github.com>
2023-09-16 21:11:00 +0530

    Add files via upload

---
+++ challenges/main.py
@@ -0,0 +1,21 @@
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+
+  #define the derived class Batsman
+class Batsman(Player):
+     def play(self):
+         print("The batsman is batting.")
+
+   #define the derived class bowler
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+  # create objects of batsman and bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# call the play() method for each object
+batsman.play()
+bowler.play()
